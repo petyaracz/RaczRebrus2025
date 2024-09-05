76698edb4b63507661987d5ef4d0df3a6038243a
Peter Racz <pracz@cogsci.bme.hu>
2024-09-05 17:08:25 +0200

    whoops

--- process.R
+++ process.R
@@ -1,7 +1,11 @@
+# -- head -- #
+
 setwd('~/Github/RaczRebrus2025/')
 
 library(tidyverse)
 library(glue)
+
+# -- fun -- #
 
 # Hungarian orthography: replace characters in digraphs with their IPA equivalents or vice versa
 transcribeIPA = function(string, direction){
@@ -13,76 +17,123 @@ transcribeIPA = function(string, direction){
   }
 }
 
+# tidy up dat
+tidyDat = function(dat){
+  dat |> 
+    mutate(
+      form_ipa = transcribeIPA(form, 'single'),
+      lemma_ipa = transcribeIPA(lemma, 'single'),
+      c_final_stem = str_detect(lemma_ipa, paste0(consonant, '$')),
+      linking_vowel_present = case_when(
+        c_final_stem & str_detect(form_ipa, paste0(vowel, suffix,'$')) ~ T,
+        c_final_stem & !str_detect(form_ipa, paste0(vowel, suffix,'$')) ~ F
+      ),
+      linking_vowel = case_when(
+        linking_vowel_present ~ str_extract(form_ipa, paste0(vowel, '(?=', suffix, '$)'))
+      ),
+      stem_final_cluster = case_when(
+        c_final_stem & !linking_vowel_present ~ str_extract(form_ipa, paste0('(?<=', vowel, ')', consonant, '+(?=', suffix, '$)')),
+        c_final_stem & linking_vowel_present ~ str_extract(form_ipa, paste0('(?<=', vowel, ')', consonant, '+(?=', linking_vowel, suffix, '$)'))
+      )
+    )
+}
+
+# get acc/def/indef data and make pairs
+getPairs = function(dat){
+  lv = dat |> 
+    filter(c_final_stem,linking_vowel_present) |> 
+    select(xpostag,lemma,form,lemma_ipa,form_ipa,stem_final_cluster,linking_vowel,freq,lemma_freq) |> 
+    rename(lv_form = form, lv_form_ipa = form_ipa, lv_freq = freq)
+  
+  nlv = dat |> 
+    filter(c_final_stem,!linking_vowel_present) |> 
+    select(lemma,form,lemma_ipa,form_ipa,freq,lemma_freq) |> 
+    rename(nlv_form = form, nlv_form_ipa = form_ipa, nlv_freq = freq)
+  
+  inner_join(lv,nlv) |> 
+    mutate(
+      lv_odds = lv_freq / nlv_freq,
+      lv_log_odds = log(lv_odds)
+    ) |> 
+    filter(lv_log_odds > -7, lv_log_odds < 7)
+}
+
+# -- var -- #
+
 vowel = '[aáeéiíoóöőuúüű]'
 consonant = '[^aáeéiíoóöőuúüű]'
 
 d = read_tsv('past_acc_hun_webcorpus2_hunspell.gz')
 
-# filter out all sorts of noise
-d = d |> 
+# -- look at three vars separately, clear up -- #
+
+## acc
+
+acc = d |> 
   filter(
-    str_detect(lemma, '(jön|tesz|van|megy|lesz)$', negate = T), # no no no
-    lemma_freq > quantile(lemma_freq, 0.001),
-    xpostag != '[/N][Pl][Acc]', # we don't want these actually
-    !(xpostag != '[/V][Pst.Def.3Sg]' & str_detect(form, 't$', negate = T)), # if this is a -t form, it should end in a -t
-    !(xpostag == '[/N][Acc]' & str_detect(form, 'tt$')), # if this is the acc, it should not end in tt
-    !(xpostag == '[/V][Pst.Def.3Sg]' & str_detect(form, 't[ae]$', negate = T)), # if this is the def past, it should end in -te/ta
-    nchar(lemma) > 2,
-    str_detect(form, '[ckgdmpb]t$', negate = T),
-    str_detect(form, paste0(consonant,'tt$'), negate = T),
-    str_detect(form, '[zt]tt[ea]$', negate = T)
-         ) |>
-  # transcribe, count syllables, def stem-final consonants
+    nchar(lemma) > 1, # sure
+    xpostag == '[/N][Acc]', # acc nouns
+    str_detect(form, 't$'), # needs to end in -t
+    str_detect(form, '[kgpbmdt]t$', negate = T) # not valid forms
+         ) |> 
   mutate(
-    form_ipa = transcribeIPA(form, 'single'),
-    lemma_ipa = transcribeIPA(lemma, 'single'),
-    nsyl = str_count(form_ipa, vowel),
-    ik_verb = str_detect(xpostag, 'V') & str_detect(lemma_ipa, 'ik$'),
+    suffix = 't'
+  ) |> 
+  tidyDat()
+
+acc_pairs = acc |> 
+  getPairs()
+
+## ndef
+
+ndef = d |> 
+  filter(
+    nchar(lemma) > 1,
+    !lemma %in% c('el','sz','gy','ly','tv','dl','ch','el','zh','év','cs','dl','cl','ár','ól','öv','egy','ház','őz','nagy','fül','agy','táv','őr','harc','erős','igen','mell','mint','nincs','gőz','ágy','ügy','alt','alj','isten','juh','kedv','parancs','száraz','ezzel','igaz','körül','szent','méz','így','órajel','méz','amil','halál','húgy','innen','jól','kapj','koszon','könyv','piac','sors','tin','toll','tél','válaszolt','úgy'),
+    xpostag == '[/V][Pst.NDef.3Sg]', # indef past
+    str_detect(form, 't$'), # needs to end in -t
+    str_detect(form, paste0(vowel, 't$'), negate = T), # this can't exist
+    str_detect(form, paste0(consonant, 'tt$'), negate = T), # nor can this
+    str_detect(form, paste0('[^oeö]tt$'), negate = T), # nor can this
+    str_detect(form, '[kgpbmd]t$', negate = T), # not valid forms # keep tt!!
+  ) |> 
+  mutate(
     suffix = case_when(
-     xpostag == '[/N][Acc]' ~ 't',
-     xpostag == '[/V][Pst.Def.3Sg]' ~ str_extract(form_ipa, 't{1,2}[ae]$'),
-     xpostag == '[/V][Pst.NDef.3Sg]' ~ str_extract(form_ipa, 't{1,2}$')
-    ),
-    stem = case_when(
-      ik_verb ~ str_replace(lemma_ipa, 'ik$', ''),
-      !ik_verb ~ lemma_ipa
-    ),
-    stem_is_c_final = str_detect(stem, paste0(consonant,'$')),
-    stem_final_cluster = str_extract(stem, paste0(consonant,'+$')),
-    stem_final_segment = str_extract(stem, paste0(consonant,'$')),
-    linking_vowel_present = stem_is_c_final & str_detect(form_ipa, paste0(vowel, suffix, '$')),
-    linking_vowel = case_when(
-      linking_vowel_present ~ str_extract(form_ipa, paste0(vowel, '(?=', suffix, '$)'))
+      str_detect(form, '[oeö]tt$') ~ 'tt',
+      str_detect(form, '[^oeöt]t$') ~ 't'
     )
-  )
+  ) |> 
+  tidyDat()
 
-d |> 
-  group_by(xpostag,linking_vowel_present) |> 
-  sample_n(2) |> 
-  ungroup() |> 
-  select(xpostag,form_ipa,stem,linking_vowel,suffix)
+ndef_pairs = ndef |> 
+  getPairs()
 
-lv = d |> 
-  filter(stem_is_c_final,linking_vowel_present) |> 
-  select(lemma,form,lemma_ipa,form_ipa,stem,stem_final_segment,stem_final_cluster,xpostag,linking_vowel,freq,lemma_freq) |> 
-  rename(lv_form = form, lv_form_ipa = form_ipa, lv_freq = freq)
+## def
 
-nlv = d |> 
-  filter(stem_is_c_final,!linking_vowel_present) |> 
-  select(lemma,form,lemma_ipa,form_ipa,xpostag,freq,lemma_freq) |> 
-  rename(nlv_form = form, nlv_form_ipa = form_ipa, nlv_freq = freq)
-
-pairs = inner_join(lv,nlv) |> 
+def = d |> 
   filter(
-    lv_freq > lemma_freq/1000, nlv_freq > lemma_freq/1000
-         ) |>
+    nchar(lemma) > 1,
+    !lemma %in% c('el','sz','gy','ly','tv','dl','ch','el','zh','év','cs','dl','cl','ár','ól','öv','egy','ház','őz','nagy','fül','agy','táv','őr','harc','erős','igen','mell','mint','nincs','gőz','ágy','ügy','alt','alj','isten','juh','kedv','parancs','száraz','ezzel','igaz','körül','szent','méz','így','órajel','méz','amil','halál','húgy','innen','jól','kapj','koszon','könyv','piac','sors','tin','toll','tél','válaszolt','úgy'),
+    xpostag == '[/V][Pst.Def.3Sg]', # indef past
+    str_detect(form, paste0(vowel, 'tt[ae]$')) | str_detect(form, paste0(consonant, 't[ae]$')), # this or this: rakta or rakotta (mi?)
+    str_detect(form, '[ptkbdgmnrlszy]tt[ae]$', negate = T) # not this one though buddy boy
+  ) |> 
   mutate(
-    lv_odds = lv_freq / nlv_freq,
-    lv_log_odds = log(lv_odds)
-         )
+    suffix = case_when(
+      str_detect(form, '[oeö]tt[ae]$') ~ str_extract(form, 'tt[ae]$'),
+      str_detect(form, '[^oeöt]t[ae]$') ~ str_extract(form, 't[ae]$')
+    )
+  ) |> 
+  tidyDat()
 
-pairs |> 
-  sample_n(5) |> 
-  select(lv_form,nlv_form)
+def_pairs = def |> 
+  getPairs()
+
+
+# -- combine -- #
+
+pairs = bind_rows(acc_pairs,def_pairs,ndef_pairs)
+
+# -- write -- #
 
 write_tsv(pairs, 'lv_n_v_pairs.tsv')
