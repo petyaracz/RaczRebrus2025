0b59e4fe71c4febfed979e4f3218466790c15414
Peter Racz <pracz@cogsci.bme.hu>
2024-09-05 10:29:57 +0200

    hmm

--- eda.R
+++ eda.R
@@ -15,6 +15,16 @@ d |>
   facet_wrap( ~ xpostag) +
   geom_point()
 
+c1 = d |> 
+  count(stem_final_segment, sort = T) |> 
+  filter(n >= 10) |> 
+  pull(stem_final_segment)
+
+c2 = d |> 
+  count(stem_final_cluster, sort = T) |> 
+  filter(n >= 10) |> 
+  pull(stem_final_cluster)
+
 s1 = d |> 
   summarise(mean = mean(lv_log_odds), .by = stem_final_segment) |> 
   mutate(stem_final_segment = fct_reorder(stem_final_segment, mean)) |> 
@@ -29,11 +39,18 @@ s2 = d |>
 
 d |> 
   mutate(stem_final_segment = factor(stem_final_segment, levels = s1)) |> 
+  filter(stem_final_segment %in% c1) |> 
   ggplot(aes(x = stem_final_segment, lv_log_odds)) +
-  geom_boxplot()
+  geom_hline(yintercept = 0, lty = 3) +
+  geom_boxplot() +
+  facet_wrap( ~ xpostag) +
+  coord_flip()
 
 d |> 
   mutate(stem_final_cluster = factor(stem_final_cluster, levels = s2)) |> 
+  filter(stem_final_cluster %in% c2) |> 
   ggplot(aes(x = stem_final_cluster, lv_log_odds)) +
+  geom_hline(yintercept = 0, lty = 3) +
   geom_boxplot() +
+  facet_wrap( ~ xpostag) +
   coord_flip()
