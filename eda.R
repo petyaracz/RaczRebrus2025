1b52b83aaa7026169b3b3073ab6b60477d23ae00
Péter Rácz <petermartonracz@gmail.com>
2024-10-18 10:52:28 +0200

    pass4

--- eda.R
+++ eda.R
@@ -5,214 +5,67 @@ setwd('~/Github/RaczRebrus2025/')
 library(tidyverse)
 library(patchwork)
 library(ggthemes)
-<<<<<<< HEAD
-library(knitr)
-library(rstanarm)
-=======
-library(lme4)
-library(sjPlot)
->>>>>>> 6d155c337a4b88ce4cef5bf13751fd7221f03205
-
-# -- fun -- #
-
-tallyC = function(dat){
-  dat |> 
-    summarise(
-      lv_freq = sum(lv_freq), 
-      nlv_freq = sum(nlv_freq),
-      .by = c(stem_final_cluster,ultimate_c,linking_vowel,xpostag,sonority_slope)
-    ) |> 
-    mutate(
-      lv_log_odds = log(lv_freq / nlv_freq)
-    )
-}
+library(glue)
 
 # -- read -- #
 
-d = read_tsv('lv_n_v_pairs.tsv')
+d = read_tsv('lv_nlv_pairs_acc.tsv')
 
-# -- lists -- #
+# -- plot -- #
 
-def_clusters = d |> 
-  filter(xpostag == '[/V][Pst.Def.3Sg]') |> 
-  distinct(stem_final_cluster) |> 
-  pull()
-
-indef_clusters = d |> 
-  filter(xpostag == '[/V][Pst.NDef.3Sg]') |> 
-  distinct(stem_final_cluster) |> 
-  pull()
-
-def = d |> 
-  filter(
-    xpostag != '[/V][Pst.NDef.3Sg]',
-    stem_final_cluster %in% def_clusters
-  )
-
-indef = d |> 
-  filter(
-    xpostag != '[/V][Pst.Def.3Sg]',
-    stem_final_cluster %in% indef_clusters
-  )
-
-# -- wrangle -- #
-
-d |> 
-  count(xpostag)
-
-d |> 
-  count(stem_final_cluster,xpostag) |> 
-  pivot_wider(names_from = xpostag, values_from = n) |> 
-<<<<<<< HEAD
-  arrange(`[/V][Pst.Def.3Sg]`,`[/V][Pst.NDef.3Sg]`) |> 
-  kable()
-=======
-  arrange(-`[/N][Acc]`)
->>>>>>> 6d155c337a4b88ce4cef5bf13751fd7221f03205
-
-s = d |> 
-  tallyC()
-
-# -- viz -- #
-
-s |> 
-  mutate(stem_final_cluster = fct_reorder(stem_final_cluster, lv_log_odds)) |> 
-  ggplot(aes(linking_vowel,lv_log_odds, group = stem_final_cluster, colour = stem_final_cluster, label = stem_final_cluster)) +
-  geom_line() +
-  geom_label() +
-  theme_few() +
-  facet_wrap( ~ xpostag) +
-  scale_colour_viridis_d() +
-  labs(colour = 'stem-final consonant(s)') +
-  xlab('linking vowel') +
-  ylab('log (linking vowel / no linking vowel)')
-
-d |> 
-  mutate(stem_final_cluster = fct_reorder(stem_final_cluster,-lv_log_odds)) |> 
-  ggplot(aes(stem_final_cluster,lv_log_odds)) +
-  geom_tufteboxplot() +
-  coord_flip() +
-  theme_bw() +
-  facet_wrap( ~ xpostag)
-
-<<<<<<< HEAD
-d |> 
-  filter(!str_detect(sonority_slope,'NA')) |> 
-  ggplot(aes(sonority_slope,lv_log_odds)) +
-  geom_tufteboxplot() +
-  facet_wrap(~ xpostag) +
-  coord_flip() +
-  theme_bw()
-
-def |> 
-  mutate(stem_final_cluster = fct_reorder(stem_final_cluster,-lv_log_odds)) |> 
-  ggplot(aes(stem_final_cluster,lv_log_odds, colour = xpostag)) +
-  geom_tufteboxplot() +
-  coord_flip() +
-  theme_bw()
-
-indef |> 
-  mutate(stem_final_cluster = fct_reorder(stem_final_cluster,-lv_log_odds)) |> 
-  ggplot(aes(stem_final_cluster,lv_log_odds, colour = xpostag)) +
-  geom_tufteboxplot() +
-  coord_flip() +
-  theme_bw()
-
-# -- lol -- #
-
-fit1 = stan_glmer(cbind(lv_freq,nlv_freq) ~ stem_final_cluster + (1|lemma), family = binomial, data = indef, cores = 4, chains = 4, iter = 2000)
-# low ess
-
-fit2 = stan_glmer(cbind(lv_freq,nlv_freq) ~ xpostag + stem_final_cluster + (1|lemma), family = binomial, data = indef, cores = 4, chains = 4, iter = 3000)
-# low ess, bayesian modelling is a joke
-fit2
-=======
-plots = d |> 
-  group_split(xpostag) |> 
-  map(~ . |> 
-        mutate(stem_final_cluster = fct_reorder(stem_final_cluster,-lv_log_odds)) |> 
-        ggplot(aes(stem_final_cluster,lv_log_odds)) +
-        geom_tufteboxplot() +
-        xlab('CC#') +
-        ylab('log(lv/no lv)') +
-        coord_flip() +
-        theme_bw() +
-        facet_wrap( ~ xpostag)
-      
-      )
-
-p1 = plots[[1]]
-p2 = plots[[2]] + theme(
-  axis.title = element_blank(),
-  axis.ticks.x = element_blank(),
-  axis.text.x = element_blank()
-  )
-p3 = plots[[3]] + theme(axis.title = element_blank())
-p1 + (p2 / p3) +
-  plot_layout(axis_titles = "collect") +
-  plot_annotation(title = 'all CC#')
-ggsave('distributions_all.png', width = 6, height = 10, dpi = 'print')
-
-plots = d |> 
-  mutate(ntiles = ntile(lemma_freq,4), .by = xpostag) |> 
-  filter(ntiles == 4) |> 
-  group_split(xpostag) |> 
-  map(~ . |> 
-        mutate(ntiles = ntile(lemma_freq,4)) |> 
-        filter(ntiles == 4) |> 
-        mutate(stem_final_cluster = fct_reorder(stem_final_cluster,-lv_log_odds)) |> 
-        ggplot(aes(stem_final_cluster,lv_log_odds)) +
-        geom_tufteboxplot() +
-        xlab('CC#') +
-        ylab('log(lv/no lv)') +
-        coord_flip() +
-        theme_bw() +
-        facet_wrap( ~ xpostag)
-      
-  )
-
-p1 = plots[[1]]
-p2 = plots[[2]] + theme(
-  axis.title = element_blank(),
-  axis.ticks.x = element_blank(),
-  axis.text.x = element_blank()
-)
-p3 = plots[[3]] + theme(axis.title = element_blank())
-p1 + (p2 / p3) +
-  plot_layout(axis_titles = "collect") +
-  plot_annotation(title = 'Q4 CC#')
-ggsave('distributions_q4.png', width = 6, height = 10, dpi = 'print')
-
-# fun with standard errors #
-
-plots = d |> 
+coda_odds = d |> 
   summarise(
-    mean = mean(lv_log_odds),
-    se = sd(lv_log_odds)/sqrt(n()),
-    .by = c(xpostag,stem_final_cluster)
-  ) |> 
-  group_split(xpostag) |> 
-  map(
-    ~ . |> 
-      mutate(stem_final_cluster = fct_reorder(stem_final_cluster,mean), .by = xpostag) |> 
-      ggplot(aes(mean,stem_final_cluster)) +
-      geom_vline(xintercept = 0, lty = 1) +
-      geom_point() +
-      geom_errorbar(aes(xmin=mean-se, xmax=mean+se), width = .25) +
-      theme_bw() +
-      xlab('mean log(linking vowel/no linking vowel)') +
-      ylab('(?<=V)C{1,2}#')
+    coda_log_odds = mean(lv_log_odds), 
+    coda_n = n(),
+    .by = coda
+    )
+
+d |>
+  left_join(coda_odds) |> 
+  filter(coda_n > 1) |> 
+  mutate(
+    coda2 = glue('{coda} ({coda_n})'),
+    coda2 = fct_reorder(coda2, coda_log_odds)
+    ) |> 
+  ggplot(aes(lv_log_odds,coda2)) +
+  geom_violin() +
+  geom_boxplot(width = .1) +
+  theme_bw() +
+  ylab('C+#') +
+  xlab('log(ízet/ízt)') +
+  scale_x_continuous(sec.axis = sec_axis('p(ízet)', transform = ~ plogis(.), breaks = c(.01,.1,.25,.5,.75,.9,.99)))
+
+
+coda_odds2 = d |> 
+  filter(!compound) |> 
+  summarise(
+    coda_log_odds = mean(lv_log_odds), 
+    coda_n = n(),
+    .by = coda
   )
 
-p1 = plots[[1]]
-p2 = plots[[2]] + theme(
-  axis.title = element_blank(),
-  axis.ticks.x = element_blank(),
-  axis.text.x = element_blank()
-)
-p3 = plots[[3]] + theme(axis.title = element_blank())
-p1 + (p2 / p3) +
-  plot_layout(axis_titles = "collect") +
-  plot_annotation(title = 'Q4 CC#', tag_levels = 'i', caption = '(i) noun.acc, (ii) verb.past.def.3sg, (iii) verb.past.ndef.3sg')
-ggsave('distributions_se.png', width = 8, height = 10, dpi = 'print')  
->>>>>>> 6d155c337a4b88ce4cef5bf13751fd7221f03205
+d |>
+  filter(!compound) |> 
+  left_join(coda_odds2) |> 
+  filter(coda_n > 1) |> 
+  mutate(
+    coda2 = glue('{coda} ({coda_n})'),
+    coda2 = fct_reorder(coda2, coda_log_odds)
+  ) |> 
+  ggplot(aes(lv_log_odds,coda2)) +
+  geom_violin() +
+  geom_boxplot(width = .1) +
+  theme_bw() +
+  ylab('C+#') +
+  xlab('log(ízet/ízt)') +
+  scale_x_continuous(sec.axis = sec_axis('p(ízet)', transform = ~ plogis(.), breaks = c(.01,.1,.25,.5,.75,.9,.99))) +
+  ggtitle('összetett szavak nélkül')
+
+d |> 
+  ggplot(aes(log_lemma_freq,lv_log_odds,colour = compound, label = lemma)) +
+  geom_label(alpha = .5) +
+  geom_smooth(alpha = .25) +
+  theme_bw() +
+  scale_colour_colorblind() +
+  xlab('log(lemmafreq)') +
+  scale_y_continuous(name = 'log(ízet/ízt)', sec.axis = sec_axis('p(ízet)', transform = ~ plogis(.), breaks = c(.01,.1,.25,.5,.75,.9,.99)))
--- setup2.R
+++ setup2.R
@@ -18,95 +18,137 @@ transcribeIPA = function(string, direction){
   }
 }
 
-# -- read -- #
+# run everything up to writing data out
+setup = function(){
 
-d = read_tsv('past_acc_hun_webcorpus2_hunspell.gz')
-load('valid_codas_siptar_torkenczy_p107.Rda')
-vowel = '[aáeéiíoóöőuúüű]'
-consonant = '[^aáeéiíoóöőuúüű]'
-
-# -- preprocess -- #
-
-d = d |> 
-  mutate(
-    stem = transcribeIPA(lemma, 'single'), # transcribe lemma, form
-    word = transcribeIPA(form, 'single'),
-    coda = str_extract(stem, glue('{consonant}+$')), # extract end of stem, form
-    eow = str_extract(word, glue('(?<={vowel}){consonant}*t$')),
-    ending = str_extract(word, glue('(?<=^{stem}).*')), # extract bit of form that's not the stem: kalap-ot, zsír-t
-    char1 = case_when( # id char1 and char2, only for 1-2 nchar codas we drop the rest
-      nchar(coda) == 2 ~ str_extract(coda, '^.')
-    ),
-    char2 = str_extract(coda, '.$')
-  )
-
-# -- filter -- #
-
-d = d |> 
-  filter(
-    xpostag == '[/N][Acc]', # noun only
-    freq > 1, # remove form hapax legomena
-    str_detect(lemma, glue('{consonant}$')), # remove vowel-final stems
-    nchar(lemma) > 2, # remove suspiciously short lemmata
-    str_detect(form, 'at', negate = T), # no lowering stems
-    nchar(coda) <= 2, # no overlong codas
-    str_detect(form, 't$'), # form has to end in -t
-    str_detect(stem, '[ptkbdgm]$', negate = T) # none of these will ever have a linking vowel
+  # -- read -- #
+  
+  d = read_tsv('past_acc_hun_webcorpus2_hunspell.gz')
+  load('valid_codas_siptar_torkenczy_p107.Rda')
+  vowel = '[aáeéiíoóöőuúüű]'
+  consonant = '[^aáeéiíoóöőuúüű]'
+  
+  # -- preprocess -- #
+  
+  d = d |> 
+    mutate(
+      stem = transcribeIPA(lemma, 'single'), # transcribe lemma, form
+      word = transcribeIPA(form, 'single'),
+      coda = str_extract(stem, glue('{consonant}+$')), # extract end of stem, form
+      eow = str_extract(word, glue('(?<={vowel}){consonant}*t$')),
+      ending = str_extract(word, glue('(?<=^{stem}).*')), # extract bit of form that's not the stem: kalap-ot, zsír-t
+      char1 = case_when( # id char1 and char2, only for 1-2 nchar codas we drop the rest
+        nchar(coda) == 2 ~ str_extract(coda, '^.')
+      ),
+      char2 = str_extract(coda, '.$')
+    )
+  
+  # -- filter -- #
+  
+  d = d |> 
+    filter(
+      xpostag == '[/N][Acc]', # noun only
+      freq > 1, # remove form hapax legomena
+      str_detect(lemma, glue('{consonant}$')), # remove vowel-final stems
+      nchar(lemma) > 2, # remove suspiciously short lemmata
+      str_detect(form, 'at', negate = T), # no lowering stems
+      nchar(coda) <= 2, # no overlong codas
+      str_detect(form, 't$'), # form has to end in -t
+      str_detect(stem, '[ptkbdgm]$', negate = T) # none of these will ever have a linking vowel
     ) |> 
     filter(coda %in% valid_codas | nchar(coda) == 1) |>  # non valid codas
     filter(nchar(coda) == 1 | char1 != char2) |> # no geminate codas
     filter(str_detect(form, glue('^{lemma}'))) # remove stem alternations (kabátot ok, egeret not ok, tornyot not ok)
+  
+  # -- linking vowel -- #
+  
+  d = d |> 
+    mutate(
+      linking_vowel = str_extract(ending, '^[^t](?=t$)'), # what's the thing before the t
+      linking_vowel_present = !is.na(linking_vowel) # is there something there
+    )
+  
+  # -- get pairs -- #
+  
+  lv = d |> 
+    filter(linking_vowel_present) |> 
+    select(lemma,form,stem,word,coda,linking_vowel,freq,lemma_freq) |> 
+    rename(lv_word = word, lv_form = form, lv_freq = freq)
+  
+  nlv = d |> 
+    filter(!linking_vowel_present) |> 
+    select(lemma,form,stem,word,coda,freq,lemma_freq) |> 
+    rename(nlv_word = word, nlv_form = form, nlv_freq = freq)
+  
+  pairs = inner_join(lv,nlv) |> 
+    mutate(
+      lv_odds = lv_freq / nlv_freq,
+      lv_log_odds = log(lv_odds)
+    )
+  
+  # -- post filtering on lv forms -- #
+  
+  pairs = pairs |> 
+    filter(abs(lv_log_odds) < 5)
+  
+  pairs |> 
+    arrange(lemma_freq) |> 
+    googlesheets4::write_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass1')
+  
+  pairs2 = googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass1')
+  
+  pairs2 = filter(pairs2, is.na(drop))
+  
+  pairs2$length = nchar(pairs2$stem)
+  
+  pairs2 |> 
+    googlesheets4::write_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass2')
+  write_tsv(pairs2, 'lv_nlv_pairs_acc.tsv')
+  
+}
 
-# -- linking vowel -- #
+# second round of hand-checking and filtering
 
-d = d |> 
-  mutate(
-    linking_vowel = str_extract(ending, '^[^t](?=t$)'), # what's the thing before the t
-    linking_vowel_present = !is.na(linking_vowel) # is there something there
-  )
+setup2 = function(){
+  
+  d = googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass2')
+  
+  # remove some more lowering stems
+  d = d |> 
+    mutate(
+      log_lemma_freq = log(lemma_freq),
+      log_freq = log((lv_freq+nlv_freq))
+    ) |> 
+    filter(
+      str_detect(lv_form, '[jnrl]et$', negate = T),
+      str_detect(lv_form, '(meszet|rezet|vizet|kezet|eszet|mézet|helyet|toronyt)$', negate = T),
+      str_detect(lv_form, '[öőüű][bcddfghjklmnprstvxy]+et', negate = T)
+    )
+  
+  # find endings for compounds
+  second_words = d |> 
+    filter(compound=="F") |> 
+    pull(stem)
+  
+  d = d |> 
+    rowwise() |> 
+    mutate(
+      word2 = str_extract(stem, paste0("(", paste(second_words, collapse = "|"), ")$"))
+    ) |> 
+    ungroup()
+  
+  d |> googlesheets4::write_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass3')
+}
 
-# -- get pairs -- #
+# -- run -- #
 
-lv = d |> 
-  filter(linking_vowel_present) |> 
-  select(lemma,form,stem,word,coda,linking_vowel,freq,lemma_freq) |> 
-  rename(lv_word = word, lv_form = form, lv_freq = freq)
+# setup()
+# setup2()
 
-nlv = d |> 
-  filter(!linking_vowel_present) |> 
-  select(lemma,form,stem,word,coda,freq,lemma_freq) |> 
-  rename(nlv_word = word, nlv_form = form, nlv_freq = freq)
-
-pairs = inner_join(lv,nlv) |> 
-  mutate(
-    lv_odds = lv_freq / nlv_freq,
-    lv_log_odds = log(lv_odds)
-  )
-
-# -- post filtering on lv forms -- #
-
-pairs = pairs |> 
-  filter(abs(lv_log_odds) < 5)
-
-pairs |> 
-  arrange(lemma_freq) |> 
-  googlesheets4::write_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass1')
-
-pairs2 = googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass1')
-
-pairs2 = filter(pairs2, is.na(drop))
-
-pairs2$length = nchar(pairs2$stem)
-
-pairs2 |> 
-  googlesheets4::write_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass2')
-write_tsv(pairs2, 'lv_nlv_pairs_acc.tsv')
-
-pairs3 = googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass2')
-
-pairs3$log_lemma_freq = log(pairs3$lemma_freq)
-pairs3$log_freq = log((pairs3$lv_freq+pairs3$nlv_freq))
+d = googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass3')
 
 # -- write -- #
 
-write_tsv(pairs3, 'lv_nlv_pairs_acc.tsv')
+d |> googlesheets4::write_sheet('https://docs.google.com/spreadsheets/d/18TpAK-rI31v7O5EVv1JD0ATsXd83lgKI-ZEZh1Q8L30/edit?usp=sharing', 'pass4')
+
+write_tsv(d, 'lv_nlv_pairs_acc.tsv')
