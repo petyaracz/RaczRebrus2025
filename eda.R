bd67fc948f9883f657e0f1e093afb4a242e8e23d
Péter Rácz <petermartonracz@gmail.com>
2024-10-18 12:18:51 +0200

    Update eda.R

--- eda.R
+++ eda.R
@@ -6,6 +6,10 @@ library(tidyverse)
 library(patchwork)
 library(ggthemes)
 library(glue)
+library(rstanarm)
+library(mgcv)
+library(sjPlot)
+library(performance)
 
 # -- read -- #
 
@@ -69,3 +73,30 @@ d |>
   scale_colour_colorblind() +
   xlab('log(lemmafreq)') +
   scale_y_continuous(name = 'log(ízet/ízt)', sec.axis = sec_axis('p(ízet)', transform = ~ plogis(.), breaks = c(.01,.1,.25,.5,.75,.9,.99)))
+
+
+# -- analysis -- #
+
+fit1 = stan_glm(cbind(lv_freq,nlv_freq) ~ 1 + log_lemma_freq + compound + length, data = d, family = binomial)
+fit2 = stan_glm(cbind(lv_freq,nlv_freq) ~ 1 + log_lemma_freq * compound + length, data = d, family = binomial)
+
+loo_compare(loo(fit1),loo(fit2))
+fit2
+fit1
+
+checks = plot(check_model(fit1, panel = F))
+checks
+
+# absolutely not.
+
+d$compound = as.factor(d$compound) # not that there is literally any indicator that this is bad
+
+gam1 = gam(cbind(lv_freq,nlv_freq) ~ s(log_lemma_freq, k = 5) + compound + s(length, k = 5), data = d, family = binomial, method = 'ML')
+plot(gam1)
+gam2 = gam(cbind(lv_freq,nlv_freq) ~ s(log_lemma_freq, by = compound, k = 5) + s(length, k = 5), data = d, family = binomial, method = 'ML')
+plot(gam2)
+summary(gam2)
+plot(compare_performance(gam1,gam2, metrics = 'common'))
+plot_model(gam2, 'pred', terms = c('log_lemma_freq','compound'))
+
+# well that was a blast
