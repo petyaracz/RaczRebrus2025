f8d543a5ce2da6eaa50362c0a22515220c35b573
Péter Rácz <petermartonracz@gmail.com>
2025-01-30 21:09:47 +0100

    some changes

--- script/eda.R
+++ script/eda.R
@@ -16,122 +16,71 @@ l = read_tsv('dat/long.tsv')
 w = read_tsv('dat/wide.tsv')
 uesz_df = read_tsv('dat/uesz_ns.tsv')
 
-# -- add info -- #
+# -- setup -- #
 
 w = w |> 
+  filter(varies) |> 
   mutate(
     neighbourhood_size = factor(neighbourhood_size, ordered = T),
     nsyl = factor(nsyl, ordered = T),
     lfpm10 = lv_lfpm10 + nlv_lfpm10,
-    koda2 = str_replace_all(coda2, c('s' = 'sz', 'š' = 's', 'ž' = 'zs'))
+    koda2 = str_replace_all(coda2, c('s' = 'sz', 'š' = 's', 'ž' = 'zs')),
+    ns = coda1 == 'n', coda2 == 'š'
   )
 
-l = l |> 
-  mutate(voiced_final_c = coda2 %in% c('z','ž'))
+w_num = w |> 
+  mutate(
+    `első "n"` = coda1 == 'n',
+    `második "s"` = coda2 == 'š',
+  ) |> 
+  rename(
+    `szomszédok\nszáma` = neighbourhood_size,
+    `log gyakoriság` = llfpm10,
+    `szótagszám` = nsyl
+  ) |> 
+  select(
+    `első "n"`,
+    `második "s"`,
+    `szomszédok\nszáma`,
+    `log gyakoriság`,
+    `szótagszám`
+  ) |> 
+  mutate(across(where(is.logical), as.numeric)) |> 
+  scale()
 
-# -- viz -- #
+# -- counts -- #
 
+# select(lemma,lv_log_odds,coda,nsyl,llfpm10bins,neighbourhood_size)
 w |> 
-  sample_n(25) |> 
-  mutate(lemma = fct_reorder(lemma, lv_log_odds)) |> 
-  ggplot(aes(lv_log_odds,lemma,colour = varies)) +
-  geom_point(pch = 21) +
-  theme_bw() +
-  theme(axis.title.y = element_blank()) +
-  scale_x_continuous('log (klienset/klienst)', sec.axis = sec_axis(~ plogis(.), name = 'p(klienset)', breaks = c(.1,.5,.9))) +
-  theme(axis.ticks = element_blank(), legend.position = 'bottom') +
-  labs(colour = 'tő') +
-  scale_colour_colorblind(labels = c('stabil', 'váltakozó'))
-
-ggsave('fig/lv_dist.png', width = 3, height = 4, dpi = 600)
-
-w |> 
-  arrange(lv_log_odds) |> 
-  select(lemma,lv_freq,nlv_freq) |> 
-  write_tsv('fig/lv_dist.tsv')
-
-p1 = w |> 
-  mutate(nsyl = fct_rev(nsyl)) |> 
-  ggplot(aes(nsyl,lv_log_odds)) +
-  geom_tufteboxplot(median.type = "line", hoffset = 0, width = 3) +
-  coord_flip() +
+  count(coda) |> 
+  mutate(coda = fct_reorder(coda,n)) |> 
+  ggplot(aes(n,coda)) +
+  geom_col() +
   theme_few() +
-  xlab('szótagszám') +
-  scale_y_continuous('log (klienset/klienst)', sec.axis = sec_axis(~ plogis(.), name = 'p(klienset)', breaks = c(.1,.5,.9))) +
-  theme(axis.ticks = element_blank())
-
-p2 = w |> 
-  mutate(neighbourhood_size = fct_rev(neighbourhood_size)) |> 
-  ggplot(aes(neighbourhood_size,lv_log_odds)) +
-  geom_tufteboxplot(median.type = "line", hoffset = 0, width = 3) +
-  coord_flip() +
-  theme_few() +
-  xlab('hangtani szomszédok\nszáma') +
-  scale_y_continuous('log (klienset/klienst)', sec.axis = sec_axis(~ plogis(.), name = 'p(klienset)', breaks = c(.1,.5,.9))) +
-  theme(axis.ticks = element_blank())
-
-p3 = w |> 
-  mutate(coda1 = fct_relevel(coda1, 'r','j','l','n')) |> 
-  ggplot(aes(coda1,lv_log_odds)) +
-  geom_tufteboxplot(median.type = "line", hoffset = 0, width = 3) +
-  coord_flip() +
-  theme_few() +
-  xlab('tővégi mássalhangzócsoport\nelső tagja') +
-  scale_y_continuous('log (klienset/klienst)', sec.axis = sec_axis(~ plogis(.), name = 'p(klienset)', breaks = c(.1,.5,.9))) +
-  theme(axis.ticks = element_blank())
-
-p4 = w |> 
-  mutate(koda2 = fct_relevel(koda2, 's','z','sz','zs')) |>
-  ggplot(aes(koda2,lv_log_odds)) +
-  geom_tufteboxplot(median.type = "line", hoffset = 0, width = 3) +
-  coord_flip() +
-  theme_few() +
-  xlab('tővégi mássalhangzócsoport\nmásodik tagja') +
-  scale_y_continuous('log (klienset/klienst)', sec.axis = sec_axis(~ plogis(.), name = 'p(klienset)', breaks = c(.1,.5,.9))) +
-  theme(axis.ticks = element_blank())
-
-p5 = w |> 
-  ggplot(aes(lv_log_odds,llfpm10)) +
-  geom_point() +
-  theme_few() +
-  ylab('lemma log gyakoriság\n10 millió tokenből') +
-  scale_x_continuous('log (klienset/klienst)', sec.axis = sec_axis(~ plogis(.), name = 'p(klienset)', breaks = c(.1,.5,.9))) +
-  theme(axis.ticks = element_blank())
+  xlab('váltakozó tövek száma') +
+  ylab('tővégi mássalhangzócsoport')
+  
+# -- log odds -- #
 
 w |> 
   mutate(
-    koda = str_replace_all(coda, c('s' = 'sz', 'š' = 's', 'ž' = 'zs')),
-    `hangtani szomszédok` = as.double(neighbourhood_size),
-    `szótagok` = as.double(nsyl)
+    coda2 = ifelse(coda == 'nš', coda, 'egyéb')
   ) |> 
-  select(lemma,`hangtani szomszédok`,`szótagok`,koda) |> 
-  pivot_longer(-c(lemma,koda)) |> 
-  ggplot(aes(value,koda)) +
-  geom_count() +
-  facet_wrap( ~ name) +
-  theme_few() +
-  ylab('tővégi mássalhangzócsoport') +
-  xlab('mennyiség')
+  ggplot(aes(coda2,lv_log_odds)) +
+  geom_rain() +
+  coord_flip() +
+  ylab('log (klienset / klienst)') +
+  xlab('tővégi mássalhangzócsoport')
 
-ggsave('fig/bigfigure2.png', dpi = 600, width = 5, height = 5)
+# -- correlations -- #
 
-(p3 + p4 + plot_spacer()) / (p1 + p2 + p5) + plot_annotation(tag_levels = 'a')
-ggsave('fig/bigfigure1.png', dpi = 600, width = 9, height = 6)
+w_pca <- prcomp(w_num, center = TRUE, scale. = TRUE)
 
-p6 = uesz_df |> 
-  ggplot(aes(year)) +
-  geom_histogram() +
-  theme_few() +
-  xlab('átvétel éve') +
-  ylab('-ns szavak száma')
+# View summary of PCA results
+summary(w_pca)
 
-uesz_df |> 
-  mutate(lemma = fct_reorder(lemma, year)) |> 
-  ggplot(aes(year,lemma)) +
-  geom_point(pch = 21) +
-  theme_bw() +
-  xlab('átvétel éve') +
-  ylab('tő')
+factoextra::fviz_pca_var(w_pca) +
+  theme_minimal() +
+  ggtitle('')
 
-ggsave('fig/ns_borrowing.png', width = 3, height = 4, dpi = 500)
-paste(uesz_df$lemma, collapse = ', ')
+ggsave('fig/pca.png', dpi = 600, width = 5, height = 5)
--- script/modelling.R
+++ script/modelling.R
@@ -14,9 +14,14 @@ library(knitr)
 
 w = read_tsv('dat/wide.tsv')
 
+# -- filt -- #
+
+w = w |> 
+  filter(varies)
+
 # -- correlations -- #
 
-w |> 
+w_num = w |> 
   mutate(
     `első "n"` = coda1 == 'n',
     `második "s"` = coda2 == 'š',
@@ -33,8 +38,21 @@ w |>
     `log gyakoriság`,
     `szótagszám`
   ) |> 
-  psych::pairs.panels(smooth = T, density = T, method = 'spearman')
+  mutate(across(where(is.logical), as.numeric)) |> 
+  scale()
 
+w_pca <- prcomp(w_num, center = TRUE, scale. = TRUE)
+
+# View summary of PCA results
+summary(w_pca)
+
+factoextra::fviz_pca_var(w_pca) +
+  theme_minimal() +
+  ggtitle('')
+                
+ggsave('fig/pca.png', dpi = 600, width = 5, height = 5)
+
+# -- rf -- #
 
 w2 = w |> 
   mutate(
@@ -55,14 +73,16 @@ w3 = w2 |>
 
 # -- rf -- #
 
-rf1 = randomForest(lv_log_odds ~ ., data = w2, mtry = 6, ntree = 1500)
+rf1 = randomForest(lv_log_odds ~ ., data = w2, mtry = 4, ntree = 2500)
 rf1
+
+rf2 = randomForest(lv_log_odds ~ ., data = w3, mtry = 1, ntree = 2500)
+rf2
+
+importance(rf2)
 importance(rf1)
 purities = pull(tibble(importance(rf1))[,1])
-
-rf2 = randomForest(lv_log_odds ~ ., data = w3, mtry = 2, ntree = 1500)
-rf2
-importance(rf2)
+purities2 = pull(tibble(importance(rf2))[,1])
 
 tibble(
   változó = c(
@@ -84,77 +104,19 @@ tibble(
   ggplot(aes(y = változó, x = `csomópont tisztaság növekedése`)) +
   geom_col() +
   theme_few()
-ggsave('fig/rf.png', width = 4, height = 5, dpi = 600)
+ggsave('fig/rf1.png', width = 4, height = 5, dpi = 600)
 
-# -- gam -- #
-
-## fit 
-
-fit1 = gam(cbind(lv_freq,nlv_freq) ~ coda1 + coda2, data = w, family = binomial)
-fit2 = gam(cbind(lv_freq,nlv_freq) ~ s(neighbourhood_size, k = 3) + s(nsyl, k = 3) + s(llfpm10), data = w, family = binomial)
-check_collinearity(fit2)
-plot(fit2)
-
-## test
-
-check_model(fit1)
-check_model(fit2)
-plot(compare_performance(fit1,fit2)) +
-  ggtitle('') +
-  labs(colour = 'additív modell') +
-  scale_colour_colorblind(labels = c('hangtani','lexikai')) +
-  scale_fill_colorblind()
-
-ggsave('fig/gamcomp.png', width = 6, height = 3, dpi = 600)
-
-test_likelihoodratio(fit1,fit2)
-
-r2_kullback(fit1)
-r2_kullback(fit2)
-
-# -- residualisation -- #
-
-# get preds
-hist(predict(fit1, type = 'link'))
-hist(predict(fit2, type = 'link'))
-
-w = w |> 
-  mutate(
-    pred_phonology = predict(fit1, type = 'link'),
-    pred_lexicon = predict(fit2, type = 'link'),
-    resid_phonology = resid(lm(pred_phonology ~ pred_lexicon, data = w)), # extremely sus
-    resid_lexicon = resid(lm(pred_lexicon ~ pred_phonology, data = w))
-  )
-
-fit1b = glm(cbind(lv_freq,nlv_freq) ~ pred_phonology + resid_lexicon, data = w, family = binomial)
-fit2b = glm(cbind(lv_freq,nlv_freq) ~ pred_lexicon + resid_phonology, data = w, family = binomial)
-
-tidy(fit1b)
-tidy(fit2b)
-
-# -- varies only -- #
-
-v = filter(w, varies)
-
-fit1c = gam(cbind(lv_freq,nlv_freq) ~ coda1 + coda2, data = v, family = binomial)
-fit2c = gam(cbind(lv_freq,nlv_freq) ~ s(neighbourhood_size, k = 3) + s(nsyl, k = 3), data = v, family = binomial)
-
-plot(compare_performance(fit1c,fit2c))
-test_performance(fit1c,fit2c)
-
-# -- viz -- #
-
-plot_model(fit1, 'pred', terms = 'coda1') + theme_few()
-plot_model(fit1, 'pred', terms = 'coda2') + theme_few()
-plot_model(fit1c, 'pred', terms = 'coda1') + theme_few()
-plot_model(fit1c, 'pred', terms = 'coda2') + theme_few()
-plot_model(fit2, 'pred', terms = 'neighbourhood_size') + theme_few()
-plot_model(fit2, 'pred', terms = 'nsyl') + theme_few()
-plot_model(fit2c, 'pred', terms = 'neighbourhood_size') + theme_few()
-plot_model(fit2c, 'pred', terms = 'nsyl') + theme_few()
-
-# -- sum -- #
-
-#  phonology model fits better than lexicon model
-# lexicon model does some work not explained by phon model, phon model does some work not explained by lexicon model
-# diachronic explanation?
+tibble(
+  változó = c(
+    "szomszédok\nszáma",
+    "log gyakoriság",
+    "szótagszám"
+  ),
+  `csomópont tisztaság növekedése` = as.double(purities2)
+) |> 
+  mutate(változó = fct_reorder(változó, -`csomópont tisztaság növekedése`)) |> 
+  ggplot(aes(y = változó, x = `csomópont tisztaság növekedése`)) +
+  geom_col() +
+  theme_few() +
+  xlim(0,200)
+ggsave('fig/rf2.png', width = 4, height = 1.5, dpi = 600)
