c60b171da3f360aa565cc4ee4f06fcacba85f90e
Péter Rácz <petermartonracz@gmail.com>
2025-01-31 17:56:53 +0100

    sklearn baby

--- script/eda.R
+++ script/eda.R
@@ -15,6 +15,19 @@ library(lme4)
 l = read_tsv('dat/long.tsv')
 w = read_tsv('dat/wide.tsv')
 uesz_df = read_tsv('dat/uesz_ns.tsv')
+varimp = read_tsv('dat/varimp.tsv')
+
+# -- example set -- #
+
+# w |> 
+#   filter(nlv_freq > 3,lv_freq > 3) |> 
+#   mutate(ntile = ntile(lv_log_odds,5)) |> 
+#   group_by(ntile) |> 
+#   sample_n(1) |> 
+#   ungroup() |> 
+#   select(lemma,llfpm10,neighbourhood_size,nsyl,lv_freq,nlv_freq,lv_odds,lv_log_odds) |>
+#   mutate(across(where(is.double), ~ round(., 2))) |> 
+#   googlesheets4::write_sheet('https://docs.google.com/spreadsheets/d/13ztdoto_RJZv-TTkju2D1bhZq7e3Z4MCkFov_cdbY5I/edit?usp=sharing', 'Sheet1')
 
 # -- setup -- #
 
@@ -43,13 +56,41 @@ w = w |>
       fct_relevel('6+','2-5','1','0')
   )
 
+# -- varimp -- #
+
+varimp = arrange(varimp, `0`)
+
+változó = c(
+  'első msh: l',
+  'második msh: zs',
+  'második msh: z',
+  'első msh: j',
+  'első msh: n',
+  'második msh: s',
+  'második msh: sz',
+  'első msh: r',
+  'szótagok száma',
+  'szomszédok száma',
+  'log gyakoriság',
+  '-ns'
+)
+
+p0 = varimp |> 
+  rename(imp = `0`) |> 
+  bind_cols(változó) |> 
+  mutate(változó = fct_reorder(változó, imp)) |> 
+  ggplot(aes(imp,változó)) +
+  geom_col() +
+  theme_bw() +
+  xlab('változó fontossága')
+
 # -- viz -- #
 
 p1 = w |> 
   mutate(lemma = fct_reorder(lemma,lv_log_odds)) |> 
   ggplot(aes(lv_log_odds,lemma, colour = ending_ns)) +
   geom_point() +
-  scale_colour_colorblind(labels = c('egyéb','-ns (docens, variáns)'), name = 'tő végződés') +
+  scale_colour_colorblind(labels = c('egyéb','-ns'), name = 'tő végződés') +
   theme_bw() +
   scale_x_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)') +
   theme(legend.position = 'bottom',axis.title.y = element_blank())
@@ -59,8 +100,9 @@ p2 = w |>
   geom_rain() +
   theme_bw() +
   coord_flip() +
-  scale_x_discrete(labels = c('egyéb','-ns (docens, variáns)'), name = 'tő végződés') +
-  scale_y_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)')
+  scale_x_discrete(labels = c('egyéb','-ns'), name = 'tő végződés') +
+  scale_y_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)') +
+  theme(axis.text.x.bottom = element_blank(), axis.ticks.x.bottom = element_blank(), axis.title.x.bottom = element_blank())
 
 p3 = w |> 
   ggplot(aes(nsize2,lv_log_odds)) +
@@ -68,7 +110,8 @@ p3 = w |>
   theme_bw() +
   coord_flip() +
   xlab('hangtani szomszédok száma') +
-  scale_y_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)')
+  scale_y_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)') +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_blank())
 
 p4 = w |> 
   ggplot(aes(nsyl2,lv_log_odds)) +
@@ -76,15 +119,17 @@ p4 = w |>
   theme_bw() +
   coord_flip() +
   xlab('tő szótagszáma') +
-  scale_y_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)')
+  scale_y_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)') +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), axis.title.x = element_blank())
 
 p5 = w |> 
   ggplot(aes(lv_log_odds,llfpm10)) +
   geom_point() +
-  geom_smooth() +
+  geom_smooth(method = 'lm') +
   theme_bw() +
   scale_x_continuous(sec.axis = sec_axis(trans = ~ plogis(.), name = 'p(klienset)', breaks = c(.01,.1,.5,.9,.99)), name = 'log (klienset / klienst)') +
-  scale_y_continuous(sec.axis = sec_axis(trans = ~ exp(.), name = 'tő gyakoriság / 10^6 szó', breaks = c(1,2,5,10,20,30)), name = 'log (tő gyakoriság / 10^6 szó)')
+  scale_y_continuous(sec.axis = sec_axis(trans = ~ exp(.), name = 'tő gyakoriság / 10^6 szó', breaks = c(1,2,5,10,20,30)), name = 'log (tő gyakoriság / 10^6 szó)') +
+  theme(axis.text.x.top = element_blank(), axis.ticks.x.top = element_blank(), axis.title.x.top = element_blank())
 
 p6 = w |> 
   mutate(
@@ -117,11 +162,14 @@ p8 = w |>
 
 # -- print -- #
 
-p1
-ggsave('fig/distro.png', dpi = 600, height = 9, width = 5)
+p0
+ggsave('fig/rf.png', dpi = 600, height = 3, width = 3)
 
-(p2 + p3) / (p4 + p5)
-ggsave('fig/relationships.png', dpi = 600, height = 6, width = 10)
+p1
+ggsave('fig/distro.png', dpi = 600, height = 9, width = 3.5)
+
+p2 / p3 / p4 / p5 + plot_annotation(tag_levels = 'i')
+ggsave('fig/relationships.png', dpi = 1200, height = 8, width = 4)
 
 p6 + (p7 / p8) + plot_layout(guides = 'collect')
 ggsave('fig/asymmetries.png', dpi = 600, height = 4, width = 6)
